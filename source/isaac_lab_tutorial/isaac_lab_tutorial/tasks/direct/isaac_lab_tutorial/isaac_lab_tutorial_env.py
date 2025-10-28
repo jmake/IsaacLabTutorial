c509bec585597d4d745f32082ba209759cd4535c
jmake <zvl_2006@hotmail.com>
2025-10-28 11:42:52 -0400

    Successfully run on cpu

--- source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env.py
@@ -59,9 +59,9 @@ class IsaacLabTutorialEnv(DirectRLEnv):
 
         self.visualization_markers = define_markers()
 
-        self.up_dir = torch.tensor([0.0, 0.0, 1.0]).cuda()  
-        self.yaws = torch.zeros((self.cfg.scene.num_envs, 1)).cuda()
-        self.commands = torch.randn((self.cfg.scene.num_envs, 3)).cuda()
+        self.up_dir = torch.tensor([0.0, 0.0, 1.0])# .cuda()  
+        self.yaws = torch.zeros((self.cfg.scene.num_envs, 1))# .cuda()
+        self.commands = torch.randn((self.cfg.scene.num_envs, 3))# .cuda()
         self.commands[:,-1] = 0.0
         self.commands = self.commands/torch.linalg.norm(self.commands, dim=1, keepdim=True)
         
@@ -74,11 +74,11 @@ class IsaacLabTutorialEnv(DirectRLEnv):
         offsets = torch.pi*plus - torch.pi*minus
         self.yaws = torch.atan(ratio).reshape(-1,1) + offsets.reshape(-1,1)
 
-        self.marker_locations = torch.zeros((self.cfg.scene.num_envs, 3)).cuda()
-        self.marker_offset = torch.zeros((self.cfg.scene.num_envs, 3)).cuda()
+        self.marker_locations = torch.zeros((self.cfg.scene.num_envs, 3))# .cuda()
+        self.marker_offset = torch.zeros((self.cfg.scene.num_envs, 3))# .cuda()
         self.marker_offset[:,-1] = 0.5
-        self.forward_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
-        self.command_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
+        self.forward_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4))# .cuda()
+        self.command_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4))# .cuda()
         
 
     def _visualize_markers(self):
@@ -134,7 +134,7 @@ class IsaacLabTutorialEnv(DirectRLEnv):
             env_ids = self.robot._ALL_INDICES
         super()._reset_idx(env_ids)
 
-        self.commands[env_ids] = torch.randn((len(env_ids), 3)).cuda()
+        self.commands[env_ids] = torch.randn((len(env_ids), 3))# .cuda()
         self.commands[env_ids,-1] = 0.0
         self.commands[env_ids] = self.commands[env_ids]/torch.linalg.norm(self.commands[env_ids], dim=1, keepdim=True)
         
